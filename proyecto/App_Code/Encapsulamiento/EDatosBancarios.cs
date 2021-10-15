e412ccfe4f9af1902c482597afa487588aeefe7d
JoseCaro <jaliriocaro@ucundinamarca.edu.co>
2021-10-15 14:47:49 -0500

    Encapsulamiento: ENegocio v1 EDatosBancarios v1 EUsuario v2 *se agregaron filas de seguridad

---
+++ proyecto/App_Code/Encapsulamiento/EDatosBancarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Serializable]
+[Table("datos_bancarios", Schema = "usuario")]
+public class EDatosBancarios
+{
+    private string cvv;
+    private string titular;
+    private string numero_tarjeta;
+    private string fecha_caducidad;
+    private string session;
+    private DateTime fecha_modificacion;
+    [Key,Column("cvv")]
+    public string Cvv { get => cvv; set => cvv = value; }
+    [Column("titular")]
+    public string Titular { get => titular; set => titular = value; }
+    [Column("numero_tarjeta")]
+    public string Numero_tarjeta { get => numero_tarjeta; set => numero_tarjeta = value; }
+    [Column("fecha_caducidad")]
+    public string Fecha_caducidad { get => fecha_caducidad; set => fecha_caducidad = value; }
+    [Column("session")]
+    public string Session { get => session; set => session = value; }
+    [Column("fecha_modificacion")]
+    public DateTime Fecha_modificacion { get => fecha_modificacion; set => fecha_modificacion = value; }
+}
--- proyecto/App_Code/Encapsulamiento/ENegocio.cs
+++ proyecto/App_Code/Encapsulamiento/ENegocio.cs
@@ -3,38 +3,23 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 [Serializable]
-[Table("usuario", Schema = "usuario")]
-public class EUsuario
+[Table("negocio", Schema = "negocio")]
+public class ENegocio
 {
-    private string cedula;
+    private string nit;
     private string nombre;
-    private string usuario;
-    private string clave;
-    private string correo;
     private string telefono;
     private string direccion;
-    private string cvv;
-    private string id_rol;
     private string session;
     private DateTime fecha_modificacion;
-    [Key,Column("cedula")]
-    public string Cedula { get => cedula; set => cedula = value; }
+    [Key,Column("nit")]
+    public string Nit { get => nit; set => nit = value; }
     [Column("nombre")]
     public string Nombre { get => nombre; set => nombre = value; }
-    [Column("usuario")]
-    public string Usuario { get => usuario; set => usuario = value; }
-    [Column("clave")]
-    public string Clave { get => clave; set => clave = value; }
-    [Column("correo")]
-    public string Correo { get => correo; set => correo = value; }
     [Column("telefono")]
     public string Telefono { get => telefono; set => telefono = value; }
     [Column("direccion")]
     public string Direccion { get => direccion; set => direccion = value; }
-    [Column("cvv")]
-    public string Cvv { get => cvv; set => cvv = value; }
-    [Column("id_rol")]
-    public string Id_rol { get => id_rol; set => id_rol = value; }
     [Column("session")]
     public string Session { get => session; set => session = value; }
     [Column("fecha_modificacion")]
