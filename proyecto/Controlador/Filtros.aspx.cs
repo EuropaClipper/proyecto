a489d27df70826e3f4feb646c6cc9df8ee0fecd4
JoseCaro <jaliriocaro@ucundinamarca.edu.co>
2021-11-07 17:12:16 -0500

    inicio de reporte de compra

--- proyecto/Controlador/Filtros.aspx.cs
+++ proyecto/Controlador/Filtros.aspx.cs
@@ -90,4 +90,50 @@ public partial class Vista_Filtros : System.Web.UI.Page
         }
         DL_Filtros.DataBind();
     }
+
+    protected void DL_Filtros_ItemCommand(object source, DataListCommandEventArgs e)
+    {
+        if (e.CommandName.ToString() == "detalles")
+        {
+            Session["producto"] = int.Parse(e.CommandArgument.ToString());
+            Response.Redirect("VistaProducto.aspx");
+        }
+        else
+        {
+            if(Session["user"] == null)
+            {
+                ClientScriptManager cm = this.ClientScript;
+                cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Inicie sesion para poder agregar productos a carrito.');</script>");
+                return;
+            }
+            //agregar a carrito
+            int id_producto = int.Parse(e.CommandArgument.ToString());
+            int cantidadReservada = int.Parse(((TextBox)e.Item.FindControl("TB_Cantidad")).Text);
+            int disponible = new DAOCarrito().CantidadDisponible(id_producto);
+            if (disponible < cantidadReservada)
+            {
+                ClientScriptManager cm = this.ClientScript;
+                cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('La cantidad seleccionada es mayor a la disponible.');</script>");
+                return;
+            }
+            ECarrito carrito = new ECarrito();
+            carrito.Cantidad = cantidadReservada;
+            carrito.FechaAgregado = DateTime.Now;
+            carrito.ProductoId = id_producto;
+            carrito.UserId = ((EUsuario)Session["user"]).Cedula;
+            ECarrito carrito_aux = new DAOCarrito().Existe(id_producto);
+            if (carrito_aux != null)
+            {
+                carrito.Id = carrito_aux.Id;
+                carrito.Cantidad = cantidadReservada + carrito_aux.Cantidad;
+                new DAOCarrito().ActualizarCarrito(carrito);
+            }
+            else
+            {
+                new DAOCarrito().InsertarCarrito(carrito);
+            }
+
+            Response.Redirect("Filtros.aspx");
+        }
+    }
 }
---
+++ proyecto/Vista/Vista_Compra.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Vista/MasterPage.master" AutoEventWireup="true" CodeFile="Vista_Compra.aspx.cs" Inherits="Vista_Vista_Compra" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table class="auto-style1">
+        <tr>
+            <td style="width:10%">&nbsp;</td>
+            <td style="width:80%">
+                <CR:CrystalReportViewer ID="CRV_Compra" runat="server" AutoDataBind="True" GroupTreeImagesFolderUrl="" Height="1202px" ReportSourceID="CRS_Compra" ToolbarImagesFolderUrl="" ToolPanelWidth="200px" Visible="False" Width="1104px" />
+                <CR:CrystalReportSource ID="CRS_Compra" runat="server">
+                    <Report FileName="C:\Users\Benzoil M\source\repos\EuropaClipper\proyecto\proyecto\Reportes\Factura.rpt">
+                    </Report>
+                </CR:CrystalReportSource>
+            </td>
+            <td style="width:10%">&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
+
